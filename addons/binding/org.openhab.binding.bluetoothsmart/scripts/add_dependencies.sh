d6311d2d76d90d97a12c76f680c93804c90ec7c3
Vlad Kolotov <vlad.kolotov@solnet.co.nz>
2017-08-04 09:56:43 +1200

    [bluetoothsmart] WIP: initial contribution Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

---
+++ addons/binding/org.openhab.binding.bluetoothsmart/scripts/add_dependencies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#cd ././../../../../../
+#pwd
+
+cp ././../../../../../sputnik/bluetooth-gatt-parser/target/*.jar ./lib/
+cp ././../../../../../sputnik/bluetooth-utils/target/*.jar ./lib/
+cp ././../../../../../sputnik/bluetooth-manager/target/*.jar ./lib/
+
---
+++ addons/binding/org.openhab.binding.bluetoothsmart/scripts/deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+scp target/org.openhab.binding.bluetoothsmart-2.2.0-SNAPSHOT.jar pi@vlad.reason.org.nz:/usr/share/openhab2/addons
